45a36cfa9e2498ab4e9dde44903dbcced9f4f0fe
Thomas <tzhenglim@gmail.com>
2024-05-07 12:21:20 +0800

    trained totaltext

--- backbones/resnet.py
+++ backbones/resnet.py
@@ -309,7 +309,7 @@ def deformable_resnet50(pretrained=True, **kwargs):
                    **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(
-            model_urls['resnet50']), strict=False)
+            model_urls['resnet50'], map_location='cpu'), strict=False)
     return model
 
 
